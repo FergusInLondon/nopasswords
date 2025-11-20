22b7e0744a5e39de6fbe3ae2e528de4e708aed2a
Fergus In London <fergus@fergus.london>
2025-11-20 23:40:37 +0000

    clients: combine and clean

--- client/src/index.ts
+++ client/src/index.ts
@@ -6,14 +6,33 @@
  * @packageDocumentation
  */
 
-export { WebAuthnClient } from './client';
+// WebAuthN Exports
+
+export { WebAuthnClient } from './webauthn/client';
 export {
   WebAuthnConfig,
-  BeginRegistrationOptions,
-  BeginAuthenticationOptions,
-  RegistrationResult,
-  AuthenticationResult,
-  BrowserCapabilities,
+  BeginRegistrationOptions as WebAuthnBeginRegistrationOptions,
+  BeginAuthenticationOptions as WebAuthnBeginAuthenticationOptions,
+  RegistrationResult as WebAuthnRegistrationResult,
+  AuthenticationResult as WebAuthnAuthenticationResult,
+  BrowserCapabilities as WebAuthnBrowserCapabilities,
   WebAuthnError,
   WebAuthnErrorType,
-} from './types';
+} from './webauthn/types';
+
+// Secure Remote Password (SRP) Exports
+
+export { SRPClient } from './srp/client';
+export { getGroup, computeK, bigIntToBytes, bytesToBigInt, padBytes } from './srp/groups';
+export type {
+  SRPConfig,
+  SRPGroup,
+  RegistrationRequest as SRPRegistrationRequest,
+  RegistrationResponse as SRPRegistrationResponse,
+  AuthenticationBeginRequest as SRPAuthenticationBeginRequest,
+  AuthenticationBeginResponse as SRPAuthenticationBeginResponse,
+  AuthenticationFinishRequest as SRPAuthenticationFinishRequest,
+  AuthenticationFinishResponse as SRPAuthenticationFinishResponse,
+  RegistrationResult as SRPRegistrationResult,
+  AuthenticationResult as SRPAuthenticationResult,
+} from './srp/types';
