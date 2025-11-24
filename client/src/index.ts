43979165b883d467aca6e50eb45acccff13abaca
Fergus In London <fergus@fergus.london>
2025-11-24 23:14:56 +0000

    js: reduce number of exports

--- client/src/index.ts
+++ client/src/index.ts
@@ -9,16 +9,3 @@
 // Secure Remote Password (SRP) Exports
 
 export { SRPClient } from './srp/client';
-export { getGroup, computeK, bigIntToBytes, bytesToBigInt, padBytes } from './srp/groups';
-export type {
-  SRPConfig,
-  SRPGroup,
-  RegistrationRequest as SRPRegistrationRequest,
-  RegistrationResponse as SRPRegistrationResponse,
-  AuthenticationBeginRequest as SRPAuthenticationBeginRequest,
-  AuthenticationBeginResponse as SRPAuthenticationBeginResponse,
-  AuthenticationFinishRequest as SRPAuthenticationFinishRequest,
-  AuthenticationFinishResponse as SRPAuthenticationFinishResponse,
-  RegistrationResult as SRPRegistrationResult,
-  AuthenticationResult as SRPAuthenticationResult,
-} from './srp/types';
